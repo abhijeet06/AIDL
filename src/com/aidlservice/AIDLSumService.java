227f27c9366114bb1b1f97412f5c42c6cf11793b
abhijeet06 <abhi.jais20@gmail.com>
2015-04-07 11:11:23 +0530

    initial commit

---
+++ src/com/aidlservice/AIDLSumService.java
@@ -0,0 +1,54 @@
+package com.aidlservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class AIDLSumService extends Service {
+
+	private static final long mSleepInterval = 1000L;
+	private String TAG = "AIDLSumService";
+	
+	public class SumServiceImpl extends ISumService.Stub {
+
+		@Override
+		public long fibonacciSum(long n) throws RemoteException {
+			Log.v(TAG  , "fibonacciSum() value of N: "+ n);
+			return AIDLSumService.this.fibonacciSum(n);
+		}
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		super.onStartCommand(intent, flags, startId);
+		return Service.BIND_AUTO_CREATE;
+	}
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return new SumServiceImpl();
+	}
+
+	public long fibonacciSum(long n){
+		Log.v(TAG  , "fibonacciSum() N: "+ n);
+		if(n == 0)
+	        return 0;
+	    else if(n == 1)
+	      return 1;
+	   else
+	      return fibonacciSum(n - 1) + fibonacciSum(n - 2);
+	}
+	
+}
